506f053d9def2cc345de7515081267e63fe9f0e8
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-14 11:16:16 -0300

    :sparkles: feat: Adiciona método estático para criptografar senhas

--- digithemis/src/utils/seguranca.py
+++ digithemis/src/utils/seguranca.py
@@ -1,2 +1,9 @@
 import bcrypt
 
+
+class Seguranca:
+    @staticmethod
+    def criptografa_senha(senha):
+        sal = bcrypt.gensalt()
+        hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), sal)
+        return hashed_senha
