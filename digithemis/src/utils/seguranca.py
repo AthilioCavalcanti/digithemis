908c80d7194d8277420fd8e8aa3aa87be7ba4cd5
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-14 11:18:11 -0300

    :sparkles: feat: Adiciona método estático para verificar senhas

--- digithemis/src/utils/seguranca.py
+++ digithemis/src/utils/seguranca.py
@@ -7,3 +7,7 @@ class Seguranca:
         sal = bcrypt.gensalt()
         hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), sal)
         return hashed_senha
+
+    @staticmethod
+    def verifica_senha(senha, hashed_senha):
+        return bcrypt.checkpw(senha.encode('utf-8'), hashed_senha)
