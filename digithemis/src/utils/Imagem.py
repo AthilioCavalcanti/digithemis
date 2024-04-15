208cf26b55d55e67f82fbe7fc060afbcae7190ce
KADS223 <kennendh@hotmail.com.br>
2024-04-15 15:29:52 -0300

    :construction: Contornos
    Aplicação dos contornos de Imagens, tanto para letras quanto das bordas.

--- digithemis/src/utils/Imagem.py
+++ digithemis/src/utils/Imagem.py
@@ -16,3 +16,16 @@ def filtro_bilateral(image):
 def segmentar(imagem):
     _, imagem_segmentada = cv2.threshold(imagem, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return imagem_segmentada
+
+def contornos_internos(imagem):
+    contours, _ = cv2.findContours(imagem, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    outline_contours = img.copy()
+    imagem_contorno = cv2.drawContours(outline_contours, contours, contourIdx=-1, color=(0, 255, 0))
+    return imagem_contorno
+
+def contornos_borda(imagem):
+    contours, _ = cv2.findContours(imagem, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    all_contours = cv2.cvtColor(imagem, cv2.COLOR_GRAY2RGB)  # Convertendo para RGB
+    contornos_borda = cv2.drawContours(all_contours, contours, -1, (0, 255, 0), 3)
+    return contornos_borda
