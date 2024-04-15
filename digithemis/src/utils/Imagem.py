0fdac208c90a9219c2c1dc6266227c9fbc593667
KADS223 <kennendh@hotmail.com.br>
2024-04-15 15:31:30 -0300

    :construction: Especificações
    Aqui são métodos que visam melhorar na manipulação de imagens para o pré-processamento.

--- digithemis/src/utils/Imagem.py
+++ digithemis/src/utils/Imagem.py
@@ -29,3 +29,43 @@ def contornos_borda(imagem):
     all_contours = cv2.cvtColor(imagem, cv2.COLOR_GRAY2RGB)  # Convertendo para RGB
     contornos_borda = cv2.drawContours(all_contours, contours, -1, (0, 255, 0), 3)
     return contornos_borda
+
+def correspondecia_modelos (imagem, template):
+    return cv2.matchTemplate(imagem, template, cv2.TM_CCOEFF_NORMED)
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype = "float32")
+
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+def four_point_transform(image, pts):
+
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
