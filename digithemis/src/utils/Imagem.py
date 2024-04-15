f83365ad5d23b4c75b1d5aeec05031c4a8c0ab98
KADS223 <kennendh@hotmail.com.br>
2024-04-15 15:28:31 -0300

    :construction: Segmentação
    :construction: a criação do método de Segmentação de imagem, usando o OTSU e Binary

--- digithemis/src/utils/Imagem.py
+++ digithemis/src/utils/Imagem.py
@@ -12,3 +12,7 @@ def Converte_cinza(image):
 
 def filtro_bilateral(image):
     return cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
+
+def segmentar(imagem):
+    _, imagem_segmentada = cv2.threshold(imagem, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return imagem_segmentada
