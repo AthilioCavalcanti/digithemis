681f47be724d54adab4455baef5c2eb98c819c19
KADS223 <kennendh@hotmail.com.br>
2024-04-15 15:27:05 -0300

    :construction: Filtro Bilateral
    :construction: construção do método do Filtro Bilateral para imagens.

--- digithemis/src/utils/Imagem.py
+++ digithemis/src/utils/Imagem.py
@@ -9,3 +9,6 @@ def plot_image(img, cmap=None):
 
 def Converte_cinza(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def filtro_bilateral(image):
+    return cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
