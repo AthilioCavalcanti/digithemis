93515ec9a33752fae8b7a34e3cab067b598b8371
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-16 09:30:37 -0300

    :sparkles: feat: Adiciona exceção de Registro Não Existente

--- digithemis/src/errors/registro_error.py
+++ digithemis/src/errors/registro_error.py
@@ -2,3 +2,9 @@ class RegistroExistenteError(Exception):
     def __init__(self, message='Registro já existente.'):
         self.mensagem = message
         super().__init__(self.mensagem)
+
+
+class RegistroNaoExistenteError(Exception):
+    def __init__(self, message='Registro não foi encontrado.'):
+        self.mensagem = message
+        super().__init__(self.mensagem)
