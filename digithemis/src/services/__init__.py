83d5b3a286bcd1b302f1699b8b6cbd6de39d827b
Carlos Moura <carlosmourapires88@gmail.com>
2024-04-15 21:25:54 -0300

    :bug: fix: insere/corrige importações nos arquivos

---
+++ digithemis/src/services/__init__.py
@@ -0,0 +1,4 @@
+from .advogado_service import AdvogadoService
+from .cliente_service import ClienteService
+from .documento_service import DocumentoService
+from .processo_service import ProcessoService
