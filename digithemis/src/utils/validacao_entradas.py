ad5764bffa90fa4e71e015eaa229c66a27146765
Carlos Moura <carlosmourapires88@gmail.com>
2024-04-17 09:30:00 -0300

    :sparkles: feat: cria método que valida senha

--- digithemis/src/utils/validacao_entradas.py
+++ digithemis/src/utils/validacao_entradas.py
@@ -10,3 +10,28 @@ class ValidacaoEntradas:
             return True
 
         return False
+    
+    @staticmethod
+    def valida_senha(senha):
+        if len(senha) < 6:
+            raise Exception('A senha deve ter no mínimo 6 caracteres')
+
+        if not re.search(r'[A-Z]', senha):
+            raise Exception(
+                'A senha deve conter pelo menos uma letra maiúscula'
+            )
+
+        if not re.search(r'[a-z]', senha):
+            raise Exception(
+                'A senha deve conter pelo menos uma letra minúscula'
+            )
+
+        if not re.search(r'\d', senha):
+            raise Exception('A senha deve conter pelo menos um dígito')
+
+        if not re.search(r'[^\w\s]', senha):
+            raise Exception(
+                'A senha deve conter pelo menos um caractere especial'
+            )
+
+        return True
