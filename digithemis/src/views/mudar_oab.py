a3843396eb7aa8aca96a9642627f71edb90b1b96
Lucas Diego Santos da Silva <lucasdigp2004@gmail.com>
2024-04-18 17:03:39 -0300

    ♻️ refactor: concluindo implementação da interface de mudar OAB.

--- digithemis/src/views/mudar_oab.py
+++ digithemis/src/views/mudar_oab.py
@@ -14,3 +14,121 @@ class ChangeOABApp(tk.Tk):
         self.iconbitmap(f'{self.relative_to_assets('favicon.ico')}')
 
         self.create_widgets()
+
+    def relative_to_assets(self, path: str) -> Path:
+        return self.assets_path / Path(path)
+    
+    def centralize(self):
+        from .app import App
+        App.centralize_app(self)
+
+    def create_widgets(self):
+        self.create_canvas()
+        self.create_entries()
+        self.create_buttons()
+
+    def create_canvas(self):
+        self.canvas = Canvas(
+            self,
+            bg='#81A69F',
+            height=641,
+            width=1007,
+            bd=0,
+            highlightthickness=0,
+            relief='ridge',
+        )
+        self.canvas.place(x=0, y=0)
+
+        # Adiciona a imagem de fundo ao canvas
+        background_image = PhotoImage(
+            file=self.relative_to_assets('mudar_oab_image_1.png')
+        )
+        self.canvas.create_image(503.0, 320.0, image=background_image)
+        self.canvas.background = (
+            background_image  # Mantém a referência à imagem de fundo
+        )
+
+        # Adiciona a segunda imagem ao canvas
+        second_image = PhotoImage(
+            file=self.relative_to_assets('mudar_oab_image_2.png')
+        )
+        self.canvas.create_image(503.0, 27.0, image=second_image)
+        self.canvas.second_image = (
+            second_image  # Mantém a referência à segunda imagem
+        )
+
+    def create_entries(self):
+        self.entry_new_oab = self.create_entry(
+            295.0, 274.0, 418.0, 45.0, 'OAB:'
+        )
+        from .app import App
+
+        self.entry_new_oab.insert(0, App.load_user_state()['oab'])
+
+    def create_entry(self, x, y, width, height, label_text):
+        entry_image = PhotoImage(
+            file=self.relative_to_assets('mudar_oab_entry_1.png')
+        )
+        entry_bg = self.canvas.create_image(504.0, y + 23.5, image=entry_image)
+        entry_frame = Frame(self, bg='#FFFFFF', padx=10)
+        entry_frame.place(x=x - 10, y=y, width=418.0, height=45.0)
+        entry = Entry(
+            self, bd=0, bg='#FFFFFF', fg='#000716', highlightthickness=0
+        )
+        entry.place(x=x, y=y, width=width, height=height)
+        self.canvas.create_text(
+            x,  # Offset para alinhar com a entrada
+            y - 20,  # Offset para alinhar com a entrada
+            anchor='nw',
+            text=label_text,
+            fill='#FFFFFF',
+            font=('HammersmithOne Regular', 15 * -1),
+        )
+        return entry
+
+    def create_buttons(self):
+        self.create_button(
+            17.0,
+            17.0,
+            20.0,
+            20.0,
+            'mudar_oab_button_1.png',
+            self.button_1_clicked,
+        )
+        self.create_button(
+            378.0,
+            366.0,
+            252.0,
+            61.0,
+            'mudar_oab_button_2.png',
+            self.button_2_clicked,
+        )
+
+    def create_button(self, x, y, width, height, image_path, command):
+        button_image = PhotoImage(file=self.relative_to_assets(image_path))
+        button = Button(
+            self,
+            image=button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=command,
+            relief='flat',
+        )
+        button.image = (
+            button_image  # Garante que a imagem seja mantida em memória
+        )
+        button.place(x=x, y=y, width=width, height=height)
+
+    def button_1_clicked(self):
+        self.destroy()
+        from .editar_perfil import EditProfileApp
+
+        edit = EditProfileApp()
+
+    def button_2_clicked(self):
+        print('button_2 clicked')
+
+
+if __name__ == '__main__':
+    app = ChangeOABApp()
+    app.mainloop()
