108d5c78640f65a1aadf3442e780b4dc2f71b49c
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-18 08:16:24 -0300

    :sparkles: feat: Adiciona exceção para senha sem letra minúscula

--- digithemis/src/errors/senha_error.py
+++ digithemis/src/errors/senha_error.py
@@ -14,3 +14,9 @@ class SenhaSemMaiuscula(ValueError):
     def __init__(self, message='A senha deve conter pelo menos uma letra maiúscula.'):
         self.mensagem = message
         super().__init__(self.mensagem)
+
+
+class SenhaSemMinusculaError(ValueError):
+    def __init__(self, message='A senha deve conter pelo menos uma letra minúscula.'):
+        self.mensagem = message
+        super().__init__(self.mensagem)
