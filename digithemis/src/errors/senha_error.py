4f520e93508b065804f3a49ae5781fcd0b5499ad
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-18 08:13:17 -0300

    :sparkles: feat: Adiciona exceção para senha sem número

--- digithemis/src/errors/senha_error.py
+++ digithemis/src/errors/senha_error.py
@@ -2,3 +2,9 @@ class SenhaCurta(ValueError):
     def __init__(self, message='A senha deve ter no mínimo 6 caracteres.'):
         self.mensagem = message
         super().__init__(self.mensagem)
+
+
+class SenhaSemNumero(ValueError):
+    def __init__(self, message='A senha deve conter pelo menos um dígito numérico.'):
+        self.mensagem = message
+        super().__init__(self.mensagem)
