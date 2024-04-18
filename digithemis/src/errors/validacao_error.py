665b003f65adc84a95b507027fd255447dc66765
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-18 07:52:29 -0300

    :sparkles: feat: Adiciona exceção para número inválido de CPF

--- digithemis/src/errors/validacao_error.py
+++ digithemis/src/errors/validacao_error.py
@@ -2,3 +2,9 @@ class NumeroProcessoInvalido(ValueError):
     def __init__(self, message='Número de processo inválido.'):
         self.mensagem = message
         super().__init__(self.mensagem)
+
+
+class CPFInvalido(ValueError):
+    def __init__(self, message='Número de CPF inválido.'):
+        self.mensagem = message
+        super().__init__(self.mensagem)
