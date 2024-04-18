1b489fdfdb50e98f1fb02121c018b03b06d49704
Lucas Diego Santos da Silva <lucasdigp2004@gmail.com>
2024-04-18 17:10:34 -0300

    ✨ feat: Implementando interface de mudar nome.

---
+++ digithemis/src/views/mudar_nome.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import tkinter as tk
+from tkinter import Canvas, Entry, Button, PhotoImage, Frame
+
+
+class ChangeNameApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.centralize()
+        self.configure(bg='#81A69F')
+        self.output_path = Path(__file__).parent
+        self.assets_path = self.output_path / 'assets'
+        self.title('Mudar nome')
+        self.iconbitmap(f'{self.relative_to_assets('favicon.ico')}')
+
+        self.create_widgets()
+
+    
