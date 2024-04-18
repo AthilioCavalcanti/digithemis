70c3856fdfeac2d732a208fe7c3801f5d6cbc1bf
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-18 08:31:42 -0300

    :recycle: refactor: Adiciona a palavra Error no nome das exceções personalizadas

--- digithemis/src/errors/validacao_error.py
+++ digithemis/src/errors/validacao_error.py
@@ -1,22 +1,22 @@
-class NumeroProcessoInvalido(ValueError):
+class NumeroProcessoInvalidoError(ValueError):
     def __init__(self, message='Número de processo inválido.'):
         self.mensagem = message
         super().__init__(self.mensagem)
 
 
-class CPFInvalido(ValueError):
+class CPFInvalidoError(ValueError):
     def __init__(self, message='Número de CPF inválido.'):
         self.mensagem = message
         super().__init__(self.mensagem)
 
 
-class CNPJInvalido(ValueError):
+class CNPJInvalidoError(ValueError):
     def __init__(self, message='Número de CNPJ inválido.'):
         self.mensagem = message
         super().__init__(self.mensagem)
 
 
-class EmailInvalido(ValueError):
+class EmailInvalidoError(ValueError):
     def __init__(self, message='Formato de e-mail inválido.'):
         self.mensagem = message
         super().__init__(self.mensagem)
