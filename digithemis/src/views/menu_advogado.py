ca00c8e59642665b8559a4f76b835e0756cc9efb
Lucas Diego Santos da Silva <lucasdigp2004@gmail.com>
2024-04-18 16:53:20 -0300

    :recycle: refactor: concluindo implementação da interface de menu do advogado

--- digithemis/src/views/menu_advogado.py
+++ digithemis/src/views/menu_advogado.py
@@ -21,4 +21,102 @@ class Menu_advogadoapp(tk.Tk):
         self.iconbitmap(f'{relative_to_assets('favicon.ico')}')
         self.mainloop()
 
-    
+    def centralize(self):
+        from .app import App
+        App.centralize_app(self)
+
+    def create_canvas(self):
+        self.canvas = Canvas(
+            self,
+            bg='#81A69F',
+            height=641,
+            width=1007,
+            bd=0,
+            highlightthickness=0,
+            relief='ridge',
+        )
+        self.canvas.place(x=0, y=0)
+
+        self.image_image_1 = PhotoImage(
+            file=relative_to_assets('advogado_image_1.png')
+        )
+        self.image_1 = self.canvas.create_image(
+            503.0, 320.0, image=self.image_image_1
+        )
+
+        self.image_image_2 = PhotoImage(
+            file=relative_to_assets('advogado_image_2.png')
+        )
+        self.image_2 = self.canvas.create_image(
+            503.0, 27.0, image=self.image_image_2
+        )
+
+    def create_buttons(self):
+        buttons_info = [
+            ('advogado_button_1.png', (437.0, 396.0), self.button_1_clicked),
+            ('advogado_button_2.png', (437.0, 209.0), self.button_2_clicked),
+            ('advogado_button_3.png', (127.0, 209.0), self.button_3_clicked),
+            ('advogado_button_4.png', (747.0, 209.0), self.button_4_clicked),
+        ]
+
+        for image_name, (x, y), command in buttons_info:
+            self.create_button(image_name, x, y, command)
+
+        self.create_button(
+            'logout.png',
+            14.0,
+            14.0,
+            self.button_5_clicked,
+        )
+
+    def create_button(self, image_name, x, y, command, **kwargs):
+        button_image = PhotoImage(file=relative_to_assets(image_name))
+        button = Button(
+            self,
+            image=button_image,
+            borderwidth=0,
+            highlightthickness=0,
+            command=command,
+            relief='flat',
+        )
+        button.image = button_image
+        button.place(x=x, y=y, **kwargs)
+
+    # Funções associadas a cada botão
+    def button_1_clicked(self):
+        self.destroy()
+        # from .editar_perfil import EditProfileApp
+
+        # editar = EditProfileApp()
+        from .perfil_advogado import AplicativoPerfilAdvogado
+        editar = AplicativoPerfilAdvogado()
+
+    def button_2_clicked(self):
+        self.destroy()
+        from .inserir_processo import InsertProcess
+        inserir_Processo = InsertProcess()
+
+    def button_3_clicked(self):
+        self.destroy()
+        from .cadastrar_cliente import RegisterClientApp
+
+        registro_cliente = RegisterClientApp()
+
+    def button_4_clicked(self):
+        self.destroy()
+        from .buscar_clientes import SearchApp
+
+        procurar = SearchApp()
+
+    def button_5_clicked(self):
+        self.destroy()
+        from .app import App
+        from .login import LoginApp
+        # logout
+        if App.user_state_exists():
+            App.delete_user_state()
+        login_tela = LoginApp()
+
+
+if __name__ == '__main__':
+    app = Menu_advogadoapp()
