c59ecbb4b447c4f0616011d518a10f0a9a9af00b
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-04-18 08:18:48 -0300

    :sparkles: feat: Adiciona exceção para senha sem caracter especial

--- digithemis/src/errors/senha_error.py
+++ digithemis/src/errors/senha_error.py
@@ -5,18 +5,32 @@ class SenhaCurta(ValueError):
 
 
 class SenhaSemNumero(ValueError):
-    def __init__(self, message='A senha deve conter pelo menos um dígito numérico.'):
+    def __init__(
+        self, message='A senha deve conter pelo menos um dígito numérico.'
+    ):
         self.mensagem = message
         super().__init__(self.mensagem)
 
 
 class SenhaSemMaiuscula(ValueError):
-    def __init__(self, message='A senha deve conter pelo menos uma letra maiúscula.'):
+    def __init__(
+        self, message='A senha deve conter pelo menos uma letra maiúscula.'
+    ):
         self.mensagem = message
         super().__init__(self.mensagem)
 
 
 class SenhaSemMinusculaError(ValueError):
-    def __init__(self, message='A senha deve conter pelo menos uma letra minúscula.'):
+    def __init__(
+        self, message='A senha deve conter pelo menos uma letra minúscula.'
+    ):
+        self.mensagem = message
+        super().__init__(self.mensagem)
+
+
+class SenhaSemCaracterEspecialError(ValueError):
+    def __init__(
+        self, message='A senha deve conter pelo menos um caractere especial.'
+    ):
         self.mensagem = message
         super().__init__(self.mensagem)
